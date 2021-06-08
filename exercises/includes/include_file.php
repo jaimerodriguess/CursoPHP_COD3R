9b5e9b1e662be0f3b119451f3d5d9b9f889f8a04
Jaime Rodriguess <jaimerodriguess@policiamilitar.sp.gov.br>
2021-06-08 10:59:36 -0300

    Alteraçao estrutura menu

---
+++ exercises/includes/include_file.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Carregando: include_file.php<br>';
+
+$var = 'Estou definida';
+
+function soma($a, $b) {
+    return $a + $b;
+}
---
+++ exercises/includes/includes01.php
@@ -0,0 +1,7 @@
+<div class="title">Includes</div>
+<?php
+echo 'Executei essa linha do arquivo includes01<br>';
+include('include_file.php');
+
+echo soma(1, 4);
+echo $var;
